e823d58bd1a59bedcd0a8f887129fb2b8de9d367
ec50n9 <shelloworld@qq.com>
2022-11-28 21:29:41 +0800

    完成列表选择功能

--- pages/shopbag/shopbag.js
+++ pages/shopbag/shopbag.js
@@ -7,7 +7,8 @@ Page({
    * 页面的初始数据
    */
   data: {
-    products: []
+    products: [],
+    isSelectAll: false
   },
 
   /**
@@ -17,15 +18,35 @@ Page({
     this.getAllShopCart()
   },
 
-  getAllShopCart(){
-    api.findAllShopcart().then(data=>{
+  getAllShopCart() {
+    api.findAllShopcart().then(data => {
       console.log(data)
-      this.setData({products: data.result})
+      const products = data.result
+      products.forEach(product => product.checked = false)
+      this.setData({
+        products
+      })
     })
   },
 
-  onCheckBoxChange(e){
-    this.setData({checked: e.detail})
+  onChangeSelectAll(e) {
+    this.setData({
+      isSelectAll: e.detail,
+      products: this.data.products.map(product => {
+        product.checked = e.detail
+        return product
+      })
+    })
+  },
+
+  onCheckBoxChange(e) {
+    console.log(e)
+    const {
+      index
+    } = e.currentTarget.dataset
+    this.setData({
+      [`products[${index}].checked`]: e.detail
+    })
   },
 
   /**

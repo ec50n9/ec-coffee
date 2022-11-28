6107f837b587abbee51f66b454eb208e166aa1a6
ec50n9 <shelloworld@qq.com>
2022-11-28 19:20:05 +0800

    增加需要登录的api接口，自动跳转到登录页面

--- utils/api.js
+++ utils/api.js
@@ -14,6 +14,26 @@ const request = (url, method = 'GET', data = {}) => new Promise(resolve => wx.re
   success: res => resolve(res.data)
 }))
 
+const requestWithLogin = (url, method = 'GET', data = {}) => {
+  try {
+    const token = wx.getStorageSync('token')
+    if (token) {
+      data.tokenString = token
+      return request(url, method, data)
+    } else {
+      goLogin()
+    }
+  } catch (e) {
+    goLogin()
+  }
+
+  function goLogin() {
+    wx.navigateTo({
+      url: '../login/login',
+    })
+  }
+}
+
 const register = (phone, password, nickName) => request('/register', 'POST', {
   nickName,
   password,
@@ -23,18 +43,44 @@ const login = (phone, password) => request('/login', 'POST', {
   phone,
   password
 })
-
-const productDetail = pid => request('/productDetail', 'GET', {
-  pid
+const search = name => request('/search', 'GET', {
+  name
 })
-
 const banner = () => request('/banner')
-
 const type = () => request('/type')
-
 const typeProducts = (key, value) => request('/typeProducts', 'GET', {
   key,
   value
+})
+const productDetail = pid => request('/productDetail', 'GET', {
+  pid
+})
+const like = pid => requestWithLogin('/like', 'POST', {
+  pid
+})
+const notLike = pid => requestWithLogin('/notlike', 'POST', {
+  pid
+})
+const findLike = pid => requestWithLogin('/findlike', 'GET', {
+  pid
+})
+const findAllLike = () => requestWithLogin('/findAllLike')
+const addShopcart = (pid, count, rules) => requestWithLogin('/addShopcart', 'POST', {
+  pid,
+  count,
+  rule: rules.join('/')
+})
+const shopcartCount = () => requestWithLogin('/shopcartCount')
+const findAllShopcart = () => requestWithLogin('/findAllShopcart')
+const modifyShopcartCount = (sid, count) => requestWithLogin('/modifyShopcartCount', 'POST', {
+  sid,
+  count
+})
+const removeShopcart = sids => requestWithLogin('/removeShopcart', 'POST', {
+  sids: JSON.stringify(sids)
+})
+const deleteShopcart = sids => requestWithLogin('/deleteShopcart', 'POST', {
+  sids: JSON.stringify(sids)
 })
 
 module.exports = {
@@ -43,5 +89,16 @@ module.exports = {
   productDetail,
   banner,
   type,
-  typeProducts
+  typeProducts,
+  search,
+  like,
+  notLike,
+  findLike,
+  findAllLike,
+  addShopcart,
+  shopcartCount,
+  findAllShopcart,
+  modifyShopcartCount,
+  removeShopcart,
+  deleteShopcart
 }
